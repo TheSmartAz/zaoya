666f46494e405af2e1f0616ec8a2abd1196ed7c4
Yongjun Zhang <emailyongjunzhang@gmail.com>
2026-01-24 14:41:34 +0800

    feat: add types index and finalize Phase 5

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,4 @@
+export type { ChatMessage, ChatState } from './chat'
+export type { InterviewOption, InterviewQuestion, InterviewGroup } from './interview'
+export type { BuildPlan } from './buildPlan'
+export type { Project, ProjectPage, DesignSystem } from './project'
